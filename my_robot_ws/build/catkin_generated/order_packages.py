59d71c266b0a3a4512c491b1661a860880f56c2d
Anan-Khalid <anan8137@gmail.com>
2024-09-12 00:10:33 +0300

    Delete my_robot_ws directory

--- my_robot_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/whitestorm/my_robot_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/whitestorm/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/whitestorm/catkin_ws/devel;/opt/ros/noetic' != '' else []
--- my_robot_ws/build/miarobot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/whitestorm/my_robot_ws/src/miarobot_description
-# Build directory: /home/whitestorm/my_robot_ws/build/miarobot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
