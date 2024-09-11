2c36ad2e9e113c75d58ebb1134b26241efef7ec3
Anan-Khalid <anan8137@gmail.com>
2024-09-11 23:51:05 +0300

    Control Goat

---
+++ my_robot_ws/build/miarobot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/whitestorm/my_robot_ws/src/miarobot_description
+# Build directory: /home/whitestorm/my_robot_ws/build/miarobot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
